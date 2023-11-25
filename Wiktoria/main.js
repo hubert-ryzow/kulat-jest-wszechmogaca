0602cbe24dbbe714e3b30d97a331f8b1a9172030
Hubert <amikyn.biznes@gmail.com>
2023-11-25 14:30:43 +0100

    .

--- Wiktoria/main.js
+++
@@ -1,5 +0,0 @@
-let photobutton = document.getElementById('photo-button')
-
-photobutton.addEventListener('click', () => {
-    location.href = 'gallery.html'
-})
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        error_reporting(0);
+        $zmienna1 = 1;
+        $zmienna2 = 2;
+
+        if($zmienna1 > $zmienna2){
+            echo("Zmienna 1 jest wieksza");
+        };
+    ?>
+</body>
+</html>
